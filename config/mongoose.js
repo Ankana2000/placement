705f67379a2b45398aad4f482575da9ff1cc03b7
your-username <your-email>
2023-09-08 11:05:54 +0530

    mongo address add

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,18 +1,12 @@
+//require library
 const mongoose = require('mongoose');
 
-const DB = 'mongodb+srv://ankanamishra5:96Pvxb4utoazEYJn@cluster0.ogxgujn.mongodb.net/?retryWrites=true&w=majority';
+//connect to database
+async function main() {
+    const db = await mongoose.connect('mongodb+srv://ankanamishra5:Ankana12356@cluster0.ogxgujn.mongodb.net/?retryWrites=true&w=majority')
+    module.exports = db;
+}
 
-mongoose.connect(DB, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'Error in connecting to MongoDB'));
-
-db.once('open', function () {
-	console.log('Connected to Database :: Mongodb');
-});
-
-module.exports = mongoose;
+main()
+.then(() => console.log('MongoDB Connected...')) //if connected
+.catch(err => console.log(err)); //if error
