95eb56a4bfcb7fdc457bfb7aa76f193f369ab5c5
your-username <your-email>
2023-09-09 17:10:22 +0530

    host

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,11 +1,5 @@
 //require library
 const mongoose = require('mongoose');
-
-<<<<<<< HEAD
-=======
-
->>>>>>> fcf551f69c8333dd0537ba482c83112a078d4da9
-//connect to database
 async function main() {
     const db = await mongoose.connect('mongodb+srv://ankumis2000:anku3421@cluster0.ahewrlp.mongodb.net/?retryWrites=true&w=majority')
     module.exports = db;
@@ -13,8 +7,4 @@ async function main() {
 
 main()
 .then(() => console.log('MongoDB Connected...')) //if connected
-<<<<<<< HEAD
 .catch(err => console.log(err)); //if error
-=======
-.catch(err => console.log(err)); //if error
->>>>>>> fcf551f69c8333dd0537ba482c83112a078d4da9
--- index.js
+++ index.js
@@ -5,15 +5,7 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const port =  8000;
-<<<<<<< HEAD
 
-
-
-
-=======
-
-
->>>>>>> fcf551f69c8333dd0537ba482c83112a078d4da9
 const app = express();
 // set ejs as view engine
 app.set('view engine', 'ejs');
